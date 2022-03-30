ea5ba7455419d3a50da18681e3ffaf5a7653dedc
Smoothzy <simon.de.bondt@hotmail.be>
2022-03-30 17:17:36 +0200

    44

--- actions/scalingChannels.js
+++ actions/scalingChannels.js
@@ -45,7 +45,7 @@ module.exports = (client) => {
             parent: "934980418489434163",
             permissionOverwrites: [
               {
-                id: newState.member.id,
+                id: 111177602818936832,
                 allow: [Permissions.FLAGS.MANAGE_CHANNELS],
               },
             ],
